c93135980177db540a1e155302f722bc95da1e6c
Arvinder <jbliss1234@hotmail.com>
2020-06-25 15:04:53 -0500

    initial checkin

---
+++ Client/Program.cs
@@ -0,0 +1,39 @@
+using System;
+using System.Net.Http;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using System.Text;
+using Microsoft.AspNetCore.Components.WebAssembly.Authentication;
+using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using blazorissuesrepro.Services;
+
+namespace blazorissuesrepro.Client
+{
+    public class Program
+    {
+        public static async Task Main(string[] args)
+        {
+            var builder = WebAssemblyHostBuilder.CreateDefault(args);
+            builder.RootComponents.Add<App>("app");
+
+            builder.Services.AddHttpClient("blazorissuesrepro.ServerAPI", client => client.BaseAddress = new Uri(builder.HostEnvironment.BaseAddress))
+                .AddHttpMessageHandler<BaseAddressAuthorizationMessageHandler>();
+
+            // Supply HttpClient instances that include access tokens when making requests to the server project
+            builder.Services.AddTransient(sp => sp.GetRequiredService<IHttpClientFactory>().CreateClient("blazorissuesrepro.ServerAPI"));
+
+            builder.Services.AddMsalAuthentication(options =>
+            {
+                builder.Configuration.Bind("AzureAd", options.ProviderOptions.Authentication);
+                options.ProviderOptions.DefaultAccessTokenScopes.Add($"{builder.Configuration["AzureAd:ClientId"]}/{builder.Configuration["AzureAd:TokenScope"]}");
+            });
+
+            builder.Services.AddScoped<AppState>();
+
+            await builder.Build().RunAsync();
+        }
+    }
+}
---
+++ Client/Services/AppState.cs
@@ -0,0 +1,38 @@
+﻿using blazorissuesrepro.Shared;
+using Microsoft.AspNetCore.Components.WebAssembly.Authentication;
+using System;
+using System.Net.Http;
+using System.Net.Http.Json;
+using System.Threading.Tasks;
+
+namespace blazorissuesrepro.Services
+{
+    public class AppState
+    {
+        private HttpClient http;
+        public AppState(HttpClient http)
+        {
+            this.http = http;
+        }
+
+        public WeatherForecast[] WeatherForecasts { get; private set; }
+
+        public event Action OnChange;
+
+        public async Task GetWeatherForecasts()
+        {
+            try
+            {
+                WeatherForecasts = await http.GetFromJsonAsync<WeatherForecast[]>("WeatherForecast");
+            }
+            catch (AccessTokenNotAvailableException exception)
+            {
+                exception.Redirect();
+            }
+
+            NotifyStateChanged();
+        }
+
+        private void NotifyStateChanged() => OnChange?.Invoke();
+    }
+}
